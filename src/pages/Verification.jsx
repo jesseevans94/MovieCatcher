00cd00b363731e1943972060780d41c93bad4035
RayanAnwer11 <mohanad.bme.ust@gmail.com>
2023-09-23 11:02:29 -0400

    push Verification

--- src/pages/Verification.jsx
+++ src/pages/Verification.jsx
@@ -1,21 +1,21 @@
 
 const Verification = () => {
 
-    // const url = 'https://api.themoviedb.org/3/authentication/guest_session/new';
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         accept: 'application/json',
-    //         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Yzc4MzgyOTIzYzdmMTZhNzRiNzliY2Y0MmRiY2I4YyIsInN1YiI6IjY1MGE0MTZlMGQ1ZDg1MDBmZGI3NTBkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5vlhHdCU3GL4v5Tirdkb84CfhgTRB-kYoOx2IotsQK0'
-    //     }
-    // }
-    // fetch(url, options)
-    //     .then(res => res.json())
-    //     .then(json => {
-    //         console.log("guest ID from verification", json.guest_session_id)
-    //         localStorage.setItem('guestID', json.guest_session_id)
-    //     })
-    //     .catch(err => console.error('error:' + err));
+    const url = 'https://api.themoviedb.org/3/authentication/guest_session/new';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Yzc4MzgyOTIzYzdmMTZhNzRiNzliY2Y0MmRiY2I4YyIsInN1YiI6IjY1MGE0MTZlMGQ1ZDg1MDBmZGI3NTBkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5vlhHdCU3GL4v5Tirdkb84CfhgTRB-kYoOx2IotsQK0'
+        }
+    }
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+            console.log("guest ID from verification", json.guest_session_id)
+            localStorage.setItem('guestID', json.guest_session_id)
+        })
+        .catch(err => console.error('error:' + err));
 
 
     return (<>Test</>)
