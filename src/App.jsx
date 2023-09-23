76c3ec621a04c9643bbd25206d6f68cd76c3c237
RayanAnwer11 <mohanad.bme.ust@gmail.com>
2023-09-23 15:56:31 -0400

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage/>}/>
       </Routes>
     </Layout>
   )
