87f5e90e8f81d6aaf3e24e4f739bf71d9bd5a93b
KoreyJamal <Koreystokes21@gmail.com>
2023-09-25 13:32:32 -0400

    Ui Improvements

--- src/api/TrendMovieItem.jsx
+++ src/api/TrendMovieItem.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import { FaHeart } from 'react-icons/fa'
+
+
 
 const TrendMovieItem = (props) => {
     const [isHovered, setIsHovered] = useState(false);
@@ -18,42 +21,52 @@ const TrendMovieItem = (props) => {
         return text;
     }
 
-    const gradientOverlayStyle = {
-        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))`,
-        position: 'absolute',
-        top: '50%',
-        left: 0,
-        right: 0,
-        height: '50%',
-        pointerEvents: 'none',
-    };
 
     return (
-        
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:max-w-7xl p-4 border-b border-gray-600">
-            <div className={`relative ${
-                isHovered ? 'overflow-visible' : 'overflow-hidden'
-            }`}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            >
-            <img className="w-full h-auto" src={`https://image.tmdb.org/t/p/w400${props.image}`} alt="" />
-            {isHovered && (
-                <div className="absolute inset-0 overflow-hidden">
-                    <div className="bg-transparent bg-opacity-80 absolute bottom-0 left-0 right-0 transform transition-transform duration-500">
-                    <p className="text-gray-white text-center px-4 py-2 font-bold text-sm">{props.overview}</p>
-                    </div>
-                </div>
-            )}
-            </div>
-            <div className="mt-2">
-                <h2 className="text-lg font-semibold">
-                    {props.title}
-                </h2>
-                <p className="text-gray-500">{props.releaseDate}</p>
-            </div>
+
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:max-w-7xl p-4 border-b border-gray-400">
+    {/* Outer container for the component */}
+    <div
+        className={`relative overflow-hidden rounded-lg shadow-lg transform transition-transform duration-300 hover:shadow-xl hover:scale-105`}
+        onMouseEnter={handleMouseEnter}  // Event handler for mouse enter
+        onMouseLeave={handleMouseLeave}  // Event handler for mouse leave
+    >
+        {/* Container for the image and overlay */}
+        <div className="h-auto rounded-lg overflow-hidden relative">
+            <img
+                className={`w-full transition-all duration-300 ${isHovered ? 'transform scale-105 blur-sm' : 'blur-none'}`}
+                src={`https://image.tmdb.org/t/p/w400${props.image}`}  // Image source URL
+                alt=""  
+            />
+            {isHovered && <div className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300"></div>}
+            {/* Overlay when the mouse is hovered */}
         </div>
-        
+
+        {isHovered && (
+            // Details displayed on mouse hover
+            <div className="absolute inset-0 flex flex-col p-4 bg-black bg-opacity-10 backdrop-blur-lg">
+                <div className="text-white">
+                    <p className="font-bold text-xl text-center mb-2">{props.title}</p>
+                    <p className="text-center text-gray-300 mb-2">Release Date: {props.releaseDate}</p>
+                </div>
+                <div className="text-gray-300 mt-auto">
+                    <p className="text-sm">{truncateText(props.overview, 100)}</p>
+                </div>
+            </div>
+        )}
+    </div>
+
+    {/* Additional movie details and actions */}
+    <div className="p-4">
+        <h1 className="text-lg text-gray-200 font-semibold mb-2">{props.title}</h1>
+        <div className="flex justify-between">
+            <p className="text-sm text-gray-600">{props.releaseDate}</p>
+            <button className=" hover:text-red-700 transition">
+                <FaHeart />
+            </button>
+        </div>
+    </div>
+</div>
 
     )
 }
--- src/api/TrendMovieList.jsx
+++ src/api/TrendMovieList.jsx
@@ -5,23 +5,31 @@ import GenreList from "../components/GenreList"
 const TrendMovieList = (props) => {
     return (
         <div className="container mx-auto sm:max-w-screen-xl">
+    {/* Heading for Trending Movies */}
     <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-white font-semibold mt-20 px-6 py-4 rounded-lg shadow-lg">
         Trending Movies
     </h1>
+
+    {/* Display a list of genres */}
     <GenreList />
+
+    {/* Horizontal line separator */}
     <div className="border-b border-gray-600 mt-4"></div>
+
+    {/* Display a list of trending movies */}
     <ul className="flex flex-wrap mx-2 md:mx-4 pt-2">
         {props.trendMovies.map((movie) => (
+            /* Display each trending movie item */
             <TrendMovieItem
-                key={movie.id}
-                title={movie.title}
-                overview={movie.overview}
-                image={movie.poster_path}
-                releaseDate={movie.release_date}
-                mediaType={movie.media_type}
-                lang={movie.original_language}
+                key={movie.id}  // Unique key for React list rendering
+                title={movie.title}  // Movie title
+                overview={movie.overview}  // Movie overview
+                image={movie.poster_path}  // Movie poster image
+                releaseDate={movie.release_date}  // Movie release date
+                mediaType={movie.media_type}  // Movie media type (e.g., movie, TV show)
+                lang={movie.original_language}  // Movie original language
                 className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 p-2"  // Adjusted width for larger screens
-                // Adjusted width for screens smaller than 640px  
+                // Adjusted width for screens smaller than 640px
             />
         ))}
     </ul>
